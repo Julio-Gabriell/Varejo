2e8258664c31a35ff906d85f7869c6179cbc5466
Julio <paespintoj@gmail.com>
2025-10-14 10:31:58 -0300

    Fix: Correção da migration removendo campo desnecessário

--- database/migrations/2025_09_30_133853_create_produtos_table.php
+++ database/migrations/2025_09_30_133853_create_produtos_table.php
@@ -9,7 +9,6 @@ return new class extends Migration {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id('idproduto');
             $table->string('nome', 100);
-            $table->decimal('valorvenda', 10, 2);
             $table->decimal('valorcompra', 10, 2);
             $table->float('precokg');
             $table->timestamps();
