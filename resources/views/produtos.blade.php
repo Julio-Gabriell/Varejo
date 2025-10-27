43beaec9b39eaf6d0935f960887dfa4ca936eef2
Julio <paespintoj@gmail.com>
2025-10-27 20:51:19 -0300

    Feat: Adição da visualização dos novos campos e da edit e delete dos produtos

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -7,14 +7,28 @@
 
     @foreach ($produtos as $produto)
             <p>
-                Nome: {{ $produto->nome }} <br>
+                Códidigo: {{ $produto->idproduto }}
             </p>
+            <img src="{{ asset('storage/' . $produto->path) }}" alt="{{ $produto->nome }}">
             <p>
-                Valor de Compra: {{ $produto->valorcompra }} <br>
+                Nome: {{ $produto->nome }} <br>
             </p>
             <p>
                 Preço do Kg: {{ $produto->precokg }} <br>
             </p>
+            <p>
+                Quantidade em estoque: {{ $produto->estoque }} <br>
+            </p>
+            <a href="{{ route('produtos.formEditar', $produto->idproduto) }}"><i class="fa-solid fa-pen"></i></a>            
+            <form action=" {{ route('produtos.deletar', $produto->idproduto) }} " method="POST"
+                onsubmit="return confirm('Tem certeza que deseja excluir o item {{ $produto->idproduto }}?')">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </form>
+            <hr>           
     @endforeach
 
     <a href="{{ route('home') }}">Home</a>
