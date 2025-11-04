bcd18b0f0f23758b9972dcccbecf095a203fb0c2
Julio <paespintoj@gmail.com>
2025-11-04 10:57:33 -0300

    Fix: Correção de nomeclaturas das classes

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,20 +7,13 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
     /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * Display a listing of the resource.
      */
     public function index()
     {
@@ -29,15 +22,53 @@ class FornecedorController extends Controller
         return view('fornecedores', compact('fornecedores'));
     }
 
-    public function ViewCriarFornecedor()
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
     {
         return view('fornecedoresForm');
     }
 
-    public function CriarFornecedor(Request $request)
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
     {
         $fornecedor = Fornecedor::create($request->all());
 
         return view('home');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
