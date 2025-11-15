b32ec4c53c570580699cce70547d86f3a32715b1
Julio <paespintoj@gmail.com>
2025-11-15 13:31:11 -0300

    Fix: Organização das rotas para melhor visualização

--- routes/web.php
+++ routes/web.php
@@ -7,39 +7,35 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('auth.login');
 });
 
 
-// Rota para redirecionar o usuário ao Google
+/*
+|--------------------------------------------------------------------------
+| AUTENTICAÇÃO VIA GOOGLE
+|--------------------------------------------------------------------------
+*/
+
+// Redireciona ao Google
 Route::get('/auth/google/redirect', function () {
     return Socialite::driver('google')->redirect();
 });
 
+// Callback do Google
 Route::get('/auth/google/callback', function () {
     $googleUser = Socialite::driver('google')->user(); // sem stateless()
 
-    // Exemplo: Criar/Logar usuário
-    $user = \App\Models\User::updateOrCreate([
-        'email' => $googleUser->getEmail(),
-    ], [
-        'name' => $googleUser->getName(),
-        'google_id' => $googleUser->getId(),
-        'avatar' => $googleUser->getAvatar(),
-        'password' => Hash::make(Str::random(16)),
-    ]);
+    $user = User::updateOrCreate(
+        ['email' => $googleUser->getEmail()],
+        [
+            'name'      => $googleUser->getName(),
+            'google_id' => $googleUser->getId(),
+            'avatar'    => $googleUser->getAvatar(),
+            'password'  => Hash::make(Str::random(16)),
+        ]
+    );
 
     Auth::login($user);
 
@@ -48,31 +44,61 @@ Route::get('/auth/google/callback', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/performance', [App\Http\Controllers\PerformanceController::class, 'index'])->name('performance');
+/*
+|--------------------------------------------------------------------------
+| ROTAS PRINCIPAIS
+|--------------------------------------------------------------------------
+*/
 
-Route::get('/produtos', [App\Http\Controllers\ProdutoController::class, 'index'])->name('produtos');
+Route::get('/home',        [App\Http\Controllers\HomeController::class,       'index'])->name('home');
+Route::get('/performance', [App\Http\Controllers\PerformanceController::class,'index'])->name('performance');
+Route::get('/produtos',    [App\Http\Controllers\ProdutoController::class,    'index'])->name('produtos');
+Route::get('/fornecedores',[App\Http\Controllers\FornecedorController::class, 'index'])->name('fornecedor');
+Route::get('/vendas',      [App\Http\Controllers\VendaController::class,      'index'])->name('vendas');
 
-Route::get('/fornecedores', [App\Http\Controllers\FornecedorController::class, 'index'])->name('fornecedor');
 
-Route::get('/vendas', [App\Http\Controllers\VendaController::class, 'index'])->name('vendas');
-    Route::get('/vendas/form', [App\Http\Controllers\VendaController::class, 'create'])->name('vendas.form');
-Route::post('/vendas/criar', [App\Http\Controllers\VendaController::class, 'store'])->name('vendas.criar');
-Route::get('/vendas/{id}/formeditar', [App\Http\Controllers\VendaController::class, 'edit'])->name('vendas.formEditar');
-Route::put('/vendas/{id}/editar', [App\Http\Controllers\VendaController::class, 'update'])->name('vendas.editar');
-Route::delete('vendas/{id}/deletar', [App\Http\Controllers\VendaController::class, 'destroy'])->name('vendas.deletar');
+/*
+|--------------------------------------------------------------------------
+| ROTAS DE VENDAS
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/vendas/form',              [App\Http\Controllers\VendaController::class, 'create'])->name('vendas.form');
+Route::post('/vendas/criar',            [App\Http\Controllers\VendaController::class, 'store'])->name('vendas.criar');
+Route::get('/vendas/{id}/formeditar',   [App\Http\Controllers\VendaController::class, 'edit'])->name('vendas.formEditar');
+Route::put('/vendas/{id}/editar',       [App\Http\Controllers\VendaController::class, 'update'])->name('vendas.editar');
+Route::delete('vendas/{id}/deletar',    [App\Http\Controllers\VendaController::class, 'destroy'])->name('vendas.deletar');
+
+
+/*
+|--------------------------------------------------------------------------
+| ROTAS RESTRITAS (ADM / GERENTE)
+|--------------------------------------------------------------------------
+*/
 
 Route::middleware('cargo:adm,gerente')->group(function () {
-    Route::get('/produtos/form', [App\Http\Controllers\ProdutoController::class, 'create'])->name('produtos.form');
-    Route::post('/produtos/criar', [App\Http\Controllers\ProdutoController::class, 'store'])->name('produtos.criar');
-    Route::get('/produtos/{id}/formeditar', [App\Http\Controllers\ProdutoController::class, 'edit'])->name('produtos.formEditar');
-    Route::put('/produtos/{id}/editar', [App\Http\Controllers\ProdutoController::class, 'update'])->name('produtos.editar');
-    Route::delete('produtos/{id}/deletar', [App\Http\Controllers\ProdutoController::class, 'destroy'])->name('produtos.deletar');
-    
-    Route::get('/fornecedores/form', [App\Http\Controllers\FornecedorController::class, 'create'])->name('fornecedor.form');
-    Route::post('/fornecedores/criar', [App\Http\Controllers\FornecedorController::class, 'store'])->name('fornecedor.criar');
-    Route::get('/fornecedores/{id}/formeditar', [App\Http\Controllers\FornecedorController::class, 'edit'])->name('fornecedor.formEditar');
-    Route::put('/fornecedores/{id}/editar', [App\Http\Controllers\FornecedorController::class, 'update'])->name('fornecedor.editar');
-    Route::delete('fornecedores/{id}/deletar', [App\Http\Controllers\FornecedorController::class, 'destroy'])->name('fornecedor.deletar');
+
+    /*
+    ----------------------------------------------------------------------
+    | PRODUTOS
+    ----------------------------------------------------------------------
+    */
+    Route::get('/produtos/form',             [App\Http\Controllers\ProdutoController::class, 'create'])->name('produtos.form');
+    Route::post('/produtos/criar',           [App\Http\Controllers\ProdutoController::class, 'store'])->name('produtos.criar');
+    Route::get('/produtos/{id}/formeditar',  [App\Http\Controllers\ProdutoController::class, 'edit'])->name('produtos.formEditar');
+    Route::put('/produtos/{id}/editar',      [App\Http\Controllers\ProdutoController::class, 'update'])->name('produtos.editar');
+    Route::delete('produtos/{id}/deletar',   [App\Http\Controllers\ProdutoController::class, 'destroy'])->name('produtos.deletar');
+
+    /*
+    ----------------------------------------------------------------------
+    | FORNECEDORES
+    ----------------------------------------------------------------------
+    */
+    Route::get('/fornecedores/form',              [App\Http\Controllers\FornecedorController::class, 'create'])->name('fornecedor.form');
+    Route::post('/fornecedores/criar',            [App\Http\Controllers\FornecedorController::class, 'store'])->name('fornecedor.criar');
+    Route::get('/fornecedores/{id}/formeditar',   [App\Http\Controllers\FornecedorController::class, 'edit'])->name('fornecedor.formEditar');
+    Route::put('/fornecedores/{id}/editar',       [App\Http\Controllers\FornecedorController::class, 'update'])->name('fornecedor.editar');
+    Route::delete('fornecedores/{id}/deletar',    [App\Http\Controllers\FornecedorController::class, 'destroy'])->name('fornecedor.deletar');
+
 });
